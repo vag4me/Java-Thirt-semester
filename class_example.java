6aa86cacc96367f50c94bb869b803e236b7d96ab
Evangelos Konstantinidis <93388918+vag4me@users.noreply.github.com>
2022-10-20 12:14:17 +0300

    Create class_example.java

---
+++ class_example.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class bike 
+{
+    int rodes = 2;
+    
+    public void show()
+    {
+        System.out.println(rodes);
+    }
+}    
+
+
+
+public class lab 
+{
+    public static void main(String[] args)
+    {
+       bike obj1 = new bike();
+        
+        obj1.show();
+    }
+}    
